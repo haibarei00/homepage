3f75cfc6611bfd1e3d5b26f2d013a5649892bf6d
LiYin <max_301@live.com>
2025-07-05 03:32:21 +0800

    fix: page functions

---
+++ functions/api/checklive.js
@@ -0,0 +1,16 @@
+export const onRequest = () => {
+    fetch('https://api.live.bilibili.com/room/v1/Room/get_info?room_id=31914326').then((res) => res.json()).then((data) => {
+        const isLive = data.data.live_status === 1;
+        return new Response(JSON.stringify({
+                live: isLive,
+                title: data.data.title
+            })
+        );
+    }).catch((error) => {
+        console.error("Error fetching live status:", error);
+        return new Response(JSON.stringify({
+            status: 500,
+            error: "Failed to fetch live status"
+        }));
+    });
+};
